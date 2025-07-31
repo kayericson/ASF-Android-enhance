635e0e0649867a92067b63e411f43fd3879a02f9
Pavel Gulin <strx1990@gmail.com>
2025-07-31 10:43:25 +0300

    Update asf-enhance.sh

--- asf-enhance.sh
+++ asf-enhance.sh
@@ -1,1 +1,13 @@
-
+# asf directory
+cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/home/noroot/asf
+# create plugins if it doesn't exist
+mkdir -p plugins
+# download  the latest ASFEnhance.zip using parsing
+curl -s https://api.github.com/repos/chr233/ASFEnhance/releases/latest \
+  | grep browser_download_url \
+  | grep ASFEnhance.zip \
+  | cut -d '"' -f 4 \
+  | xargs wget
+unzip ASFEnhance.zip -d plugins
+# delete zip
+rm ASFEnhance.zip
